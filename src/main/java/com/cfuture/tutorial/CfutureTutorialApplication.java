50fec3ba1f8d03dc121430eacbf29bbfe95a7965
klmin <haemmas@naver.com>
2024-09-05 20:24:56 +0900

    a

---
+++ src/main/java/com/cfuture/tutorial/CfutureTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.cfuture.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfutureTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfutureTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cfuture/tutorial/CfutureTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfuture.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfutureTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
